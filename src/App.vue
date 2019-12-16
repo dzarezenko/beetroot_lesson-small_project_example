49b0da8c0a695ae02f4d61b356491e0b6ce2ad21
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-16 20:50:32 +0200

    Fixes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,59 @@
 <template>
-  <div id="app">
-    
-  </div>
+  <section id="home">
+      <section id="bg">
+        <header>
+          <div id="logo"><img src="images/Logo.png" alt="logo" /></div>
+
+          <main-menu />
+          
+          <div class="socialMenu">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+          </div>
+        </header>
+        <div id="mainSlider">
+          <div
+            style="background-image: url(images/tu-tram-pham-202263.png);   background-size: cover;
+          height: 100vh; background-repeat: no-repeat;"
+          >
+            <h1>SIMPLE &amp; MODERN</h1>
+            <p>WE MAKE THE WORLD'S BEAUTIFUL EVERY DAY</p>
+          </div>
+          <div
+            style="background-image: url(images/scott-webb-176434.png);  background-size: cover;
+          height: 100vh; background-repeat: no-repeat;"
+          >
+            <h1>One World Trade Center</h1>
+            <p>WE MAKE THE WORLD'S BEAUTIFUL EVERY DAY</p>
+          </div>
+          <div
+            style="background-image: url(images/main-slider2.png);  background-size: cover;
+          height: 100vh; background-repeat: no-repeat;"
+          >
+            <h1>International Commerce Centre</h1>
+            <p>WE MAKE THE WORLD'S BEAUTIFUL EVERY DAY</p>
+          </div>
+          <div>
+            <h1>Effective Advertising Pointers</h1>
+            <p>WE MAKE THE WORLD'S BEAUTIFUL EVERY DAY</p>
+          </div>
+          <div>
+            <h1>Hypnotize Yourself Into The Ghost</h1>
+            <p>WE MAKE THE WORLD'S BEAUTIFUL EVERY DAY</p>
+          </div>
+        </div>
+      </section>
+      <a href="#gallery" class="scroll"><i class="fas fa-chevron-down"></i></a>
+    </section>
 </template>
+
+<script>
+import Menu from "@/components/menu/Menu";
+
+export default {
+  components: {
+    'main-menu': Menu,
+  }
+}
+</script>
---
+++ src/components/menu/Menu.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <a href="#whatWeDo" class="scroll">ABOUT US</a>
+    <a href="#gallery" class="scroll">PROJECTS</a>
+    <a href="#news" class="scroll">NEWS</a>
+    <a href="#contact" class="scroll">CONTACT</a>
+  </nav>
+</template>
