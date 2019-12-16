a2d032113dd60dfdfbe443c20d9711051be946e7
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-16 20:57:16 +0200

    Menu component added

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,8 +1,22 @@
 <template>
   <nav>
-    <a href="#whatWeDo" class="scroll">ABOUT US</a>
-    <a href="#gallery" class="scroll">PROJECTS</a>
-    <a href="#news" class="scroll">NEWS</a>
-    <a href="#contact" class="scroll">CONTACT</a>
+    <a v-for="(menuItem, key) in menu"
+       :key="key" :href="menuItem.url" class="scroll">{{ menuItem.title }}</a>
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        { title: "ABOUT US", url: "#whatWeDo" },
+        { title: "PROJECTS", url: "#gallery" },
+        { title: "NEWS", url: "#news" },
+        { title: "CONTACT", url: "#contact" },
+        { title: "TEST", url: "#test" },
+      ]
+    }
+  }
+}
+</script>
