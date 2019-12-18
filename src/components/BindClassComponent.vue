3e9e2b7eaaf3a09ef7ef1891272e638f01af54c6
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-18 19:40:16 +0200

    Dynamic class and style attributes binding

---
+++ src/components/BindClassComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="sqr" :style="css"
+   :class="className" @click="changeColor()"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      className: {
+        redBg: true,
+        blueBg: false,
+      },
+      css: [
+        {
+          width: '100px',
+          height: '100px',
+          borderWidth: '3px',
+        },
+        {
+          borderStyle: 'solid',
+          borderColor: "yellow",
+        },
+      ]
+    }
+  },
+  methods: {
+    changeColor(e) {
+      this.className.redBg = !this.className.redBg;
+      this.className.blueBg = !this.className.blueBg;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.redBg {
+  background-color: red;
+}
+.greenBg {
+  background-color: green;
+}
+.blueBg {
+  background-color: blue;
+}
+</style>
